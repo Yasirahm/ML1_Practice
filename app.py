4907dcf5183272c656bc9b92cdf7f28f5df43c88
Yasirahm <ratherseemu16@gmail.com>
2026-06-22 16:18:38 +0530

    fixed routing

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import request
 
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,9 @@ def predict():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
